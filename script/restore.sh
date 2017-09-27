3d5de9ad1e7aa48108e6c1ca2e77746dabdb2b10
Ryo Nakae <ryo.n.1129@gmail.com>
2017-09-27 13:07:11 +0900

    dataとwp-cliサービスを追加 & 環境変数読み込み

--- script/restore.sh
+++ script/restore.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # restore wp-content/uploads
-docker-compose run --rm data tar xvf /backup/wp-content-uploads.tar
+docker-compose run --rm data tar -xvf /backup/wp-content-uploads.tar
 
 # restore db by wp-cli
 docker-compose run --rm wpcli db import /backup/db.sql
