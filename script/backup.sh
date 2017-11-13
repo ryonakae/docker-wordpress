b14842f729b93187e59a4a40dc4fd0940a1bdeed
Ryo Nakae <ryo.n.1129@gmail.com>
2017-11-13 12:56:31 +0900

    update script

--- script/backup.sh
+++ script/backup.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+# remove backup files
+docker-compose run --rm data rm -rf /backup/wp-content-uploads.tar /backup/db.sql
+
 # backup wp-content/uploads
 docker-compose run --rm data tar -cvf /backup/wp-content-uploads.tar /var/www/html/wp-content/uploads
 
--- script/restore.sh
+++ script/restore.sh
@@ -6,5 +6,8 @@ docker-compose run --rm data tar -xvf /backup/wp-content-uploads.tar
 # restore db by wp-cli
 docker-compose run --rm wpcli db import /backup/db.sql
 
+# remove backup files
+docker-compose run --rm data rm -rf /backup/wp-content-uploads.tar /backup/db.sql
+
 # restart
 docker-compose restart
